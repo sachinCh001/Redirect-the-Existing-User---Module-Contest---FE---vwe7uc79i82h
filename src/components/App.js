dc17449a35439f2722f905972a264d5a652d5d7a
sachinCh001 <121339822+sachinCh001@users.noreply.github.com>
2023-04-16 10:33:12 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react';
 import '../styles/App.css';
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import LoginForm from './LoginForm';
 import Dashboard from './Dashboard';
 
 const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   return (
     <div id="main">
       <Router>
-          <LoginForm />
-          <Dashboard />
+        <Switch>
+          <Route exact path="/">
+            {isLoggedIn ? <Redirect to="/dashboard" /> : <LoginForm setIsLoggedIn={setIsLoggedIn} />}
+          </Route>
+          <Route path="/dashboard">
+            {!isLoggedIn ? <Redirect to="/" /> : <Dashboard setIsLoggedIn={setIsLoggedIn} />}
+          </Route>
+        </Switch>
       </Router>
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
